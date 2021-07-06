a6383a14ff8d5d191b61e6167d05c733f96229a2
Caroline Fassot <fassot.caroline@gmail.com>
2021-07-06 14:34:16 +0200

    fix link anchor

--- src/router/index.js
+++ src/router/index.js
@@ -112,7 +112,12 @@ const routes = [
 const router = new VueRouter({
 	mode: 'history',
 	base: process.env.BASE_URL,
-	routes
+	routes,
+	scrollBehavior: function(to) {
+		if(to.hash) {
+			return {selector: to.hash}
+		}
+	},
 })
 
 
